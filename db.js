186458a8f1be19e4772c8e881c9d2f08a777ebaf
Ayu Aisyah <88034909+AyuAisyahFatimatuhSyahra@users.noreply.github.com>
2022-03-01 12:33:09 +0700

    Add files via upload

---
+++ db.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const mysql = require("mysql")
+
+//koneksi
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "node_spp"
+})
+
+module.exports = db;
---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict'
+
+//inisialisasi
+const express = require("express")
+
+//implementasi
+const app = express()
+app.use(express.json())
+
+//menghubungkan ke database
+const db = require('./db')
+db.connect(error =>{
+    if(error){
+        console.log(error.message)
+    }else{
+        console.log("Mysql Connected")
+    }
+} )
+
+//endpoint
+app.get("/", (req,res) => {
+    res.send({
+        message: "Berhasil menjalankan GET",
+        data:{
+            description :
+            "Endpoint ini menampilkan data"
+        }
+    })
+})
+
+
+app.use("/penduduk", require('./routes/penduduk.route'))
+
+const port = 8080
+app.listen(port, () => console.log (`App sudah berjalan ${port}`))
