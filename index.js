d97dda23a282b5013d88dba31ada327bbecf47e0
Ayu Aisyah <88034909+AyuAisyahFatimatuhSyahra@users.noreply.github.com>
2022-03-03 12:13:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,29 +7,31 @@ const express = require("express")
 const app = express()
 app.use(express.json())
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended:false}))
+
 //menghubungkan ke database
 const db = require('./db')
 db.connect(error =>{
-    if(error){
-        console.log(error.message)
-    }else{
-        console.log("Mysql Connected")
-    }
-} )
+    if(error) throw error
+    console.log("mysql connected")
+})
 
 //endpoint
 app.get("/", (req,res) => {
     res.send({
-        message: "Berhasil menjalankan GET",
-        data:{
+        message:"Berhasil menjalankan GET",
+        data: {
             description :
-            "Endpoint ini menampilkan data"
+            "berhasil menampilkan data"
         }
     })
 })
 
+app.use("/auth",require('./routes/login.route'))
+app.use("/admin",require('./routes/admin.route'))
+app.use("/petugas",require('./routes/petugas.route'))
+app.use("/siswa",require('./routes/siswa.route'))
 
-app.use("/penduduk", require('./routes/penduduk.route'))
-
-const port = 8080
-app.listen(port, () => console.log (`App sudah berjalan ${port}`))
+const port = 8080;
+app.listen(port, () => console.log (`App berhasil berjalan di ${port}`))
